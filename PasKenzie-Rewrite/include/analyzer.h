90a546aee4bd5067ffa87c3c2e4f0a560f7ff4ca
Lucas Fernandes de Camargo <135182612+Lucas-FcNw@users.noreply.github.com>
2025-09-28 21:35:14 -0300

    Delete PasKenzie-Rewrite directory

--- PasKenzie-Rewrite/include/analyzer.h
+++
@@ -1,9 +0,0 @@
-#ifndef ANALYZER_H
-#define ANALYZER_H
-#include "core.h"
-
-void pkz_accept(TokenKind expected);
-void pkz_analyze();
-void pkz_parse_program();
-
-#endif // ANALYZER_H
--- PasKenzie-Rewrite/include/scanner.h
+++
@@ -1,8 +0,0 @@
-#ifndef SCANNER_H
-#define SCANNER_H
-#include "core.h"
-
-void pkz_scan_init(char *buffer);
-Token pkz_next();
-
-#endif // SCANNER_H
